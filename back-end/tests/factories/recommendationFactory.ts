002a5837f78ada094f859d96565e4e51e09b048c
ramon oliveira <ramon30012000@gmail.com>
2022-05-07 04:52:43 -0300

    tests: add post on /recommendations and post on /recommendations/:id/upvote integration tests

---
+++ back-end/tests/factories/recommendationFactory.ts
@@ -0,0 +1,6 @@
+export default function recommendationFactory() {
+  return {
+    name: "sadness and sorrow",
+    youtubeLink: "https://www.youtube.com/watch?v=wEWF2xh5E8s"
+  };
+}
--- back-end/tests/integration/recomendation.test.ts
+++
@@ -1,3 +0,0 @@
-describe("POST /recommendations", () => {
-  it.todo("should return 201 and persist the recommendation given a valid recommendation");
-});
