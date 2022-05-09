8afc5263c4c20326556a1761db0e354db6013c5d
ramon oliveira <ramon30012000@gmail.com>
2022-05-09 03:28:05 -0300

    tests: add unit tests to the service layer

--- back-end/tests/unit/factories/recommendationFactory.ts
+++ back-end/tests/unit/factories/recommendationFactory.ts
@@ -2,7 +2,7 @@ import faker from "@faker-js/faker";
 
 export default function recommendationFactory() {
   return {
-    id: 1,
+    id: Number(faker.random.numeric),
     name: faker.lorem.words(2),
     youtubeLink: "https://www.youtube.com/watch?v=wEWF2xh5E8s",
     score: 0
--- back-end/tests/unit/recommendationService.test.ts
+++ back-end/tests/unit/recommendationService.test.ts
@@ -1,4 +1,5 @@
 import { jest } from "@jest/globals";
+import { PrismaPromise, Recommendation } from "@prisma/client";
 
 import { recommendationRepository } from "../../src/repositories/recommendationRepository.js";
 import { recommendationService } from "../../src/services/recommendationsService.js";
@@ -23,7 +24,7 @@ describe("Recommendation Service Unit Tests", () => {
   });
   
   describe("downvote", () => {
-    it("should delete the recommendation given it score is less than -5", async () => {
+    it("should delete the recommendation given its score is less than -5", async () => {
       const recommendation = recommendationFactory();
 
       jest.spyOn(recommendationRepository, "find").mockResolvedValueOnce(recommendation);
@@ -37,8 +38,56 @@ describe("Recommendation Service Unit Tests", () => {
     });
   });
 
+  describe("getRandom", () => {
+    const recommendations = [
+      { ...recommendationFactory(), score: 9 },
+      { ...recommendationFactory(), score: 9 },
+      { ...recommendationFactory(), score: 20 },
+      { ...recommendationFactory(), score: 20 },
+    ];
+
+    jest.spyOn(recommendationRepository, "findAll").mockImplementation(
+      ({ score, scoreFilter }): PrismaPromise<Recommendation[]> => {
+        if (scoreFilter === "gt") {
+          return new Promise(resolve => {
+            resolve(recommendations.filter(r => r.score > score));
+          }) as PrismaPromise<Recommendation[]>; 
+        }
+        return new Promise(resolve => {
+          resolve(recommendations.filter(r => r.score < score));
+        }) as PrismaPromise<Recommendation[]>; 
+      }
+    );
+
+    it("should throw a not found error given it has no recommendation", async () => {
+      jest.spyOn(recommendationRepository, "findAll")
+        .mockResolvedValueOnce([])
+        .mockResolvedValueOnce([]);
+      
+      expect(async () => {
+        await recommendationService.getRandom();
+      }).rejects.toEqual(notFoundError());
+    });
+
+    it("should return recommendation with score greater than 10 given random is smaller than 0.7 ", async () => {      
+      jest.spyOn(Math, "random").mockReturnValue(0.6);
+      
+      const randomRecommendation = await recommendationService.getRandom();
+
+      expect(randomRecommendation.score).toBeGreaterThan(10);
+    });
+    
+    it("should return recommendation with score smaller than 10 given random is greater than 0.7 ", async () => {      
+      jest.spyOn(Math, "random").mockReturnValue(0.9);
+      
+      const randomRecommendation = await recommendationService.getRandom();
+
+      expect(randomRecommendation.score).toBeLessThanOrEqual(10);
+    });
+  });
+
   describe("getByIdOrFail", () => {
-    it("should throw a not found error given an id from a unexistent recommendation", async () => {
+    it("should throw a not found error given an id from an unexistent recommendation", async () => {
       const recommendation = recommendationFactory();
 
       jest.spyOn(recommendationRepository, "find").mockResolvedValueOnce(undefined);
